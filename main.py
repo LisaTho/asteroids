1c7d41cc75a37bc1acf550ebd5c71d6d39cd2824
LisaTho <lisathoennessen@gmail.com>
2025-02-19 18:08:26 +0100

    Configured max fps

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init
+    uhu = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
                 return
         screen.fill((1, 1, 1))
         pygame.display.flip()
+        dt = uhu.tick(60)/1000
 
 
 
