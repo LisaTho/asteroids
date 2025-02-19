c794e1bfce543e9c14ca2c598048ecef518964e4
LisaTho <lisathoennessen@gmail.com>
2025-02-19 18:53:58 +0100

    draw ship

--- main.py
+++ main.py
@@ -3,22 +3,27 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init
+    pygame.init()
     uhu = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player_ship = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((1, 1, 1))
-        pygame.display.flip()
+        player_ship.draw(screen)
+        pygame.display.flip()        
         dt = uhu.tick(60)/1000
 
 
