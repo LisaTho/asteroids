01e51abfa971e0159b50be53e45165e5dae819f9
LisaTho <lisathoennessen@gmail.com>
2025-02-19 17:58:22 +0100

    basic screen setup

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
-    print(SCREEN_WIDTH)
-    print(SCREEN_HEIGHT)
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill((1, 1, 1))
+        pygame.display.flip()
 
 
 
